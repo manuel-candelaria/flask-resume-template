625034b741903fc669feb0b3232a5028c7e8759b
manuel-candelaria <lorenzo.candelaria@gmail.com>
2023-03-12 12:51:36 +0000

    Changes to be committed: 	new file:   restart.sh

---
+++ restart.sh
@@ -0,0 +1,12 @@
+# Stop flask-resume-template SERVICE
+# Stop nginx server
+sudo systemctl stop flask-resume-template
+sudo systemctl stop  nginx
+
+# restar service
+#restart nginx
+sudo systemctl daemon-reload
+sudo systemctl start flask-resume-template
+sudo systemctl enable flask-resume-template
+sudo systemctl start nginx
+sudo systemctl enable nginx

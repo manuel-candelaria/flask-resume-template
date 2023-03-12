4df3e3779edb96f05e842d84814a821461fe6991
dr_calavera <lorenzo.candelaria@gmail.com>
2023-03-12 12:27:03 +0100

    Updates to index, added images, updates to YAML

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from flask import render_template
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-
 @app.route('/')
 def index():
     website_data = yaml.full_load(open('_config.yaml'))
 
     return render_template('index.html', data=website_data)
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
